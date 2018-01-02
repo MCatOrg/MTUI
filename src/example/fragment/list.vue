7850797728a0d5af927ec02b06c1ef0de2e56f23
SugarTiger <1029109173@qq.com>
2018-01-02 09:47:28 +0800

    list

--- src/example/fragment/list.vue
+++ src/example/fragment/list.vue
@@ -21,16 +21,22 @@
 
 
 
-    <div class="mtui-cells__title">复选列表</div>
-    <div class="mtui-cells">
-      <div class="mtui-cell">
-        <div>5555</div>
-        <div>6666</div>
-      </div>
-      <div class="mtui-cell">
-        <div>7777</div>
-        <div>8888</div>
-      </div>
+    <div class="mtui-cells__title">多选列表项</div>
+    <div class="mtui-cells mtui-cells_radio">
+      <label class="mtui-cell mtui-check__label" for="mt1">
+        <div class="mtui-cell__bd">多选选择1</div>
+        <div class="mtui-cell__ft">
+          <input type="radio" class="mtui-check" name="radio1" id="mt1" checked="checked">
+          <span class="mtui-icon-checked"></span>
+        </div>
+      </label>
+      <label class="mtui-cell mtui-check__label" for="mt2">
+        <div class="mtui-cell__bd">多选选择2</div>
+        <div class="mtui-cell__ft">
+          <input type="radio" class="mtui-check" name="radio1" id="mt2">
+          <span class="mtui-icon-checked"></span>
+        </div>
+      </label>
     </div>
   </div>
 </template>
