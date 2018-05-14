9c75149aa8bb7bc2963e059b78387180b32094a5
SugarTiger <1029109173@qq.com>
2018-05-14 15:34:02 +0800

    第一提交

---
+++ examples/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="index">
+    <button @click="showT">显示</button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    showT() {
+      this.$Toast('测试用例3', {
+        time: 2000,
+        className: 'afei',
+        callback() {},
+      });
+    },
+  },
+};
+</script>
+
---
+++ packages/components/toast/toast.vue
@@ -0,0 +1,32 @@
+<template>
+  <transition name="fade">
+    <div :class="className" v-show="visible">
+      <div class="mtui-mask_transparent"></div>
+      <div class="mtui-toast">
+          <i class="mtui-icon_toast mtui-icon-success-no-circle"></i>
+          <p class="mtui-toast__content">{{content}}</p>
+      </div>
+    </div>
+  </transition>
+</template>
+<script>
+export default {
+  name: 'toast',
+  data() {
+    return {
+      time: 2 * 1000,
+      className: '',
+      content: '',
+      visible: false,
+    };
+  },
+};
+</script>
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
