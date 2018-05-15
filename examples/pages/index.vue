4db18b387f2525d4e59fdf21a9664e70073d9422
blak-kong <546598185@qq.com>
2018-05-15 11:11:02 +0800

    按钮

--- examples/pages/index.vue
+++ examples/pages/index.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="index">
     <button @click="showT">显示</button>
+    <mtui-button type="default" size="small">12345</mtui-button>
   </div>
 </template>
 <script>
+import MtuiButton from '../../packages/components/button/button';
 export default {
+  components: {
+      MtuiButton,
+  },
   methods: {
     showT() {
       this.$Toast('测试用例3', {
