5076f693fa22e3096f6addbf2c95d070d916ab3b
SugarTiger <1029109173@qq.com>
2018-05-22 18:39:20 +0800

    loadmore

--- examples/pages/loadmore.vue
+++ examples/pages/loadmore.vue
@@ -1,32 +1,74 @@
 <template>
   <div id="mtLoadMore">
-    <mt-loadmore>
-      <div class="proList">
-        <div v-for="(pro,i) of proList" :key="i" class="item">
-          <img :src="pro" alt="">
-          <div>
-            <h2>产品{{i+1}}</h2>
-            <p>价格</p>
+    <!-- 父组件高度必须固定，且overflow-y:scroll -->
+    <div class="page-loadmore-wrapper" ref="wrapper" :style="{height:`${wrapperHeight}px`}">
+      <mt-loadmore
+       ref="loadmore"
+      :top-method="topMethod"
+      :bottom-method="bottomMethod"
+      @translate-change="translateChange"
+      :bottom-all-loaded="bottomAllLoaded"
+      top-loading-text="">
+        <div class="proList">
+          <div v-for="(pro,i) of proList" :key="i" class="item">
+            <img :src="pro" alt="">
+            <div>
+              <h3>产品{{i+1}}</h3>
+              <p>价格</p>
+            </div>
           </div>
         </div>
-      </div>
-    </mt-loadmore>
+      </mt-loadmore>
+      <p v-if="bottomAllLoaded">已经到底部</p>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      proList: [
+      proList: [],
+      source: ['https://avatars3.githubusercontent.com/u/19727413?s=40&v=4',
         'https://avatars3.githubusercontent.com/u/19727413?s=40&v=4',
         'https://avatars3.githubusercontent.com/u/19727413?s=40&v=4',
+        'https://avatars3.githubusercontent.com/u/19727413?s=40&v=4'],
+      row: ['https://avatars3.githubusercontent.com/u/19727413?s=40&v=4',
         'https://avatars3.githubusercontent.com/u/19727413?s=40&v=4',
         'https://avatars3.githubusercontent.com/u/19727413?s=40&v=4',
-        'https://avatars3.githubusercontent.com/u/19727413?s=40&v=4',
-        'https://avatars3.githubusercontent.com/u/19727413?s=40&v=4',
-        'https://avatars3.githubusercontent.com/u/19727413?s=40&v=4',
-      ],
+        'https://avatars3.githubusercontent.com/u/19727413?s=40&v=4'],
+      wrapperHeight: 0,
+      bottomAllLoaded: false,
     };
+  },
+  mounted() {
+    // 必须先设置高度
+    const DW = document.documentElement.clientHeight;
+    this.wrapperHeight = DW - this.$refs.wrapper.getBoundingClientRect().top;
+    console.dir(this.$refs.wrapper.style.height);
+    setTimeout(() => {
+      this.proList = this.source.slice(0); // 模拟异步
+    }, 200);
+  },
+  methods: {
+    topMethod() {
+      setTimeout(() => {
+        this.proList = this.source.slice(0);
+        this.bottomAllLoaded = false;
+        this.$refs.loadmore.onTopLoaded();
+      }, 2000);
+    },
+    translateChange() {
+    },
+    bottomMethod() {
+      setTimeout(() => {
+        if (this.proList.length > 30) {
+          this.bottomAllLoaded = true;
+        } else {
+          this.proList = this.proList.concat(this.row);
+        }
+        this.$refs.loadmore.onBottomLoaded();
+      }, 2000);
+    },
   },
 };
 </script>
@@ -46,6 +88,9 @@ export default {
       height: 1rem;
     }
   }
+}
+.page-loadmore-wrapper{
+  overflow-y: scroll;
 }
 </style>
 
