3799d6a5279855d196eb9c5663c22b6dc5db6707
blak-kong <546598185@qq.com>
2018-05-25 13:49:43 +0800

    未完成

--- examples/pages/index.vue
+++ examples/pages/index.vue
@@ -1,33 +1,6 @@
 <template>
   <div id="index">
-<<<<<<< HEAD
     <button @click="showT">显示</button>
-    <mtui-button type="default" size="small">12345</mtui-button>
-<<<<<<< HEAD
-=======
-    <mtui-cell title="标题文字" label="描述信息"><span>icon 是图片</span></mtui-cell>
-    <mtui-cell title="标题文字">555</mtui-cell>
-    <!-- <mtui-radio></mtui-radio> -->
->>>>>>> cell
-  </div>
-</template>
-<script>
-import MtuiButton from '../../packages/components/button/button';
-<<<<<<< HEAD
-export default {
-  components: {
-      MtuiButton,
-=======
-// import MtuiRadio from '../../packages/components/radio/radio';
-import MtuiCell from '../../packages/components/cell/cell';
-export default {
-  components: {
-      MtuiButton,
-      MtuiCell,
-      // MtuiRadio
->>>>>>> cell
-  },
-=======
     <button @click="showT">Toast</button>
     <router-link to="/button" class="mtui-cell mtui-cell__link">
             <div class="mtui-cell__bd">Button</div>
@@ -62,7 +35,6 @@ export default {
 <script>
 
 export default {
->>>>>>> 'slider'
   methods: {
     showT() {
       this.$Toast('测试用例3', {
