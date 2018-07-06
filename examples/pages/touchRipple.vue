ba52f47c4bf941c13fdb0f653c32c7655e9af8e9
SugarTiger <1029109173@qq.com>
2018-07-06 14:03:13 +0800

    "version": "1.3.3"

--- examples/pages/touchRipple.vue
+++ examples/pages/touchRipple.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
-    <mt-touch-ripple id="mr" color="#fff">
+    <mt-touch-ripple id="mr" color="#fff" @click="send">
       我就是我
     </mt-touch-ripple>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    send() {
+      alert('hello');
+    },
+  },
+};
+</script>
+
 <style lang="less" scoped>
 #mr{
   position: relative;
